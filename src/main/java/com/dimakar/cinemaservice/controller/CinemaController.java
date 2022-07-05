8e9676165dd223c434dec8f6510f3e3311cbdddf
dimakar <makarovskii@phystech.edu>
2022-07-05 22:56:29 +0300

    add ResponseStatusException

--- src/main/java/com/dimakar/cinemaservice/controller/CinemaController.java
+++ src/main/java/com/dimakar/cinemaservice/controller/CinemaController.java
@@ -2,17 +2,16 @@ package com.dimakar.cinemaservice.controller;
 
 import com.dimakar.cinemaservice.dto.Cinema;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 public class CinemaController {
     private final List<Cinema> cinemaList = new ArrayList<>();
+
     {
         cinemaList.add(new Cinema(1, "Sokolniki", "Stromynka", true));
         cinemaList.add(new Cinema(2, "VDNH", "Mira", false));
@@ -24,39 +23,36 @@ public class CinemaController {
     }
 
     @GetMapping("/cinema/{id}")
-    public ResponseEntity<Cinema> getCinemaById(@PathVariable int id) {
+    public Cinema getCinemaById(@PathVariable int id) {
         if (cinemaList.stream().anyMatch(cinema -> cinema.getId().equals(id))) {
-            return new ResponseEntity<>(cinemaList.stream().filter(cinema -> cinema.getId().equals(id))
+            return cinemaList.stream().filter(cinema -> cinema.getId().equals(id))
                     .findFirst()
-                    .orElseThrow(() -> new NoSuchElementException("Cinema with id = '" + id + "' is not found")), HttpStatus.OK);
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cinema with id = '" + id + "' is not found"));
         } else {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cinema with id ='" + id + "' is not found'");
         }
     }
 
     @PostMapping("/cinema")
-    public ResponseEntity<Cinema> createCinema(@RequestBody Cinema newCinema) {
+    public Cinema createCinema(@RequestBody Cinema newCinema) {
         if (newCinema.getId() == null || newCinema.getName() == null) {
-            return new ResponseEntity<>(cinemaList.stream().findFirst()
-                    .orElseThrow(() -> new InvalidParameterException("Id and name mustn't be null")), HttpStatus.BAD_REQUEST);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id and name mustn't be null");
         }
         if (cinemaList.stream().anyMatch(cinema -> cinema.getId().equals(newCinema.getId()))) {
-            return new ResponseEntity<>(cinemaList.stream().findFirst()
-                    .orElseThrow(() -> new InvalidParameterException("Cinema with id = '" + newCinema.getId() + "' already exists")), HttpStatus.CONFLICT);
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Cinema with id = '" + newCinema.getId() + "' already exists");
         } else {
             cinemaList.add(newCinema);
-            return new ResponseEntity<>(newCinema, HttpStatus.CREATED);
+            return newCinema;
         }
     }
 
     @DeleteMapping("/cinema/{id}")
-    public ResponseEntity createCinema(@PathVariable int id) {
+    public void createCinema(@PathVariable int id) {
         if (cinemaList.stream().anyMatch(cinema -> cinema.getId().equals(id))) {
             cinemaList.remove(cinemaList.stream().filter(cinema -> cinema.getId().equals(id))
                     .findFirst().orElseThrow());
-            return new ResponseEntity<>(HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Cinema with id ='" + id + "' is not found'");
         }
     }
 
